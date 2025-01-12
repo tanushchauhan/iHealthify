c8bb595dd37b5e86d921aae8a8ed2526a99fb6b3
Tanush Chauhan <tanushchauhan07@gmail.com>
2025-01-11 18:05:15 -0600

    Initial Commit

---
+++ iHealthify/iHealthifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  iHealthifyApp.swift
+//  iHealthify
+//
+//  Created by Tanush Chauhan on 1/11/25.
+//
+
+import SwiftUI
+
+@main
+struct iHealthifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
